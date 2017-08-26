38a6a70682a4b827e816f220058198100cca05c2
“Tarciso <tarcisoftorres@g4flex.com.br>
2017-08-26 18:34:00 -0300

    First Commit

---
+++ app/Http/Controllers/BotController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarciso
+ * Date: 8/16/17
+ * Time: 8:54 PM
+ */
+namespace App\Http\Controllers;
+
+use CodeBot\CallSendApi;
+use CodeBot\Message\Text;
+use CodeBot\SenderRequest;
+use CodeBot\WebHook;
+use Illuminate\Http\Request;
+
+class BotController extends Controller
+{
+    public function subscribe(){
+        $webhook = new WebHook;
+        $subscribe = $webhook->check(config('botfb.validationToken'));
+        if(!$subscribe)
+        {
+            abort(403, 'Unauthorized action.');
+        }
+
+        return $subscribe;
+    }
+
+    public function receiveMessage(Request $request)
+    {
+        $sender = new SenderRequest();
+        $senderId = $sender->getSenderId();
+        $message = $sender->getMessage();
+
+        $text = new Text($senderId);
+        $callSenderApi = new CallSendApi(config('botfb.pageAccessToken'));
+
+        $callSenderApi->make($text->message('Oii, eu sou um bot...'));
+        $callSenderApi->make($text->message('Você digitou: '. $message));
+    }
+}
---
+++ config/botfb.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tarciso
+ * Date: 8/17/17
+ * Time: 8:25 AM
+ */
+
+return [
+    'pageAccessToken' => env('FB_PAGE_ACCESS_TOKEN'),
+    'validationToken' => env('FB_VALIDATION_TOKEN'),
+];
