f9cdba96fd839e6b97b19a9eafa0040b105587fa
“Tarciso <tarcisoftorres@g4flex.com.br>
2017-09-16 17:37:34 -0300

    trocado a forma de gerar logs

--- config/app.php
+++ config/app.php
@@ -120,7 +120,8 @@ return [
     |
     */
 
-    'log' => env('APP_LOG', 'single'),
+    /*'log' => env('APP_LOG', 'single'),*/
+    'log' => 'errorlog',
 
     'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
